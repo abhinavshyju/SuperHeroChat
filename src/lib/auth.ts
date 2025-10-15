2a82cf779345d3810401cd7075b1f0341d9df8e8
Abhinav shyju <abhinavshyjupc@gmail.com>
2025-10-15 13:14:18 +0530

    fix : env json file load issue

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,21 @@
-import authConfig from "@/config/auth.config";
+import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import NextAuth from "next-auth";
+import authConfig from "@/config/auth.config";
+
+const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT!);
+
+const app = !getApps().length
+  ? initializeApp({
+      credential: cert(serviceAccount),
+    })
+  : getApp();
+
+const firestore = getFirestore(app);
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  adapter: FirestoreAdapter(),
+  adapter: FirestoreAdapter(firestore),
   session: { strategy: "jwt" },
   ...authConfig,
 });
